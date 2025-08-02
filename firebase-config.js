1b5e4d00330cbfa5f153a93023da8fb1061334ca
Manumatrix1 <luciano21martinez@gmail.com>
2025-08-02 11:06:23 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,9 +9,9 @@ const firebaseConfig = {
     apiKey: "AIzaSyDhcJyTriTBrEDftDx_lYAhFgd6Y8Us4dA",
     authDomain: "papirodigital-1a6c5.firebaseapp.com",
     projectId: "papirodigital-1a6c5",
-    storageBucket: "papirodigital-1a6c5.appspot.com",
-    messagingSenderId: "274494841235", // ¡LA CORRECTA!
-    appId: "1:274494841235:web:99112dde1c6dbd12240866" // ¡LA CORRECTA!
+    storageBucket: "papirodigital-1a6c5.firebasestorage.app", // ¡CORREGIDO!
+    messagingSenderId: "274494841235",
+    appId: "1:274494841235:web:99112dde1c6dbd12240866"
 };
 
 // Inicializa Firebase y exporta los servicios que necesitas
