2c27caecaa6411ce10700488a0bb31f1db9752de
Manumatrix1 <luciano21martinez@gmail.com>
2025-08-04 00:44:03 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,26 +1,25 @@
-// firebase-config.js
+// Importa las funciones que necesitas desde las URLs completas del SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// Importa las funciones que necesitas de los SDKs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js";
-
-// TODO: Reemplaza lo siguiente con la configuración de tu propio proyecto de Firebase
-// Encuentra estos datos en tu Consola de Firebase > Configuración del proyecto
+// La configuración de tu aplicación web de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDhcJyTriTBrEDftDx_lYAhFgd6Y8Us4dA",
+  authDomain: "papirodigital-1a6c5.firebaseapp.com",
+  databaseURL: "https://papirodigital-1a6c5-default-rtdb.firebaseio.com",
+  projectId: "papirodigital-1a6c5",
+  storageBucket: "papirodigital-1a6c5.appspot.com",
+  messagingSenderId: "274494841235",
+  appId: "1:274494841235:web:99112dde1c6dbd12240866",
+  measurementId: "G-0SKBCYEEF0"
 };
 
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta los servicios que usarás en otros archivos para que puedan ser importados
+// Inicializa los servicios y los exporta para poder usarlos en otros archivos de tu proyecto
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
