08e5b686fa2294ed0776b8d87e4c96ab40f35832
Manumatrix1 <luciano21martinez@gmail.com>
2025-08-03 22:05:42 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,25 @@
-// firebase-config.js
+// Importa las funciones que necesitas de los SDKs
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js";
-
-// TODO: Reemplaza lo siguiente con la configuración de tu proyecto de Firebase
+// La configuración de tu aplicación web de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDhcJyTriTBrEDftDx_lYAhFgd6Y8Us4dA", // Reemplaza si generaste una nueva
+  authDomain: "papirodigital-1a6c5.firebaseapp.com",
+  databaseURL: "https://papirodigital-1a6c5-default-rtdb.firebaseio.com",
+  projectId: "papirodigital-1a6c5",
+  storageBucket: "papirodigital-1a6c5.appspot.com",
+  messagingSenderId: "274494841235",
+  appId: "1:274494841235:web:99112dde1c6dbd12240866",
+  measurementId: "G-0SKBCYEEF0"
 };
 
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta los servicios que usarás
+// Inicializa y exporta los servicios para que otros archivos puedan usarlos
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
